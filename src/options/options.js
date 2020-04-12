98362301f46b258d0f75789f9ce874e2345ec35b
IvarssonAndreas <Ivarsson.Andreas@hotmail.se>
2020-04-12 17:39:32 +0200

    extract Google analytics setup to own folder and function

--- src/options/options.js
+++ src/options/options.js
@@ -3,32 +3,9 @@ import createDefaultList from '../components/createDefaultList';
 import createTimerList from '../components/createTimerList';
 import createGeneralSwitch from '../components/createGeneralSwitch';
 import createNavigation from '../components/createNavigation';
+import setUpGoogleAnalytics from "../analytics"
 
-(function (i, s, o, g, r, a, m) {
-  i.GoogleAnalyticsObject = r;
-  (i[r] =
-    i[r] ||
-    function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(
-  window,
-  document,
-  'script',
-  'https://www.google-analytics.com/analytics.js',
-  'ga'
-);
-
-ga('create', 'UA-162836929-1', 'auto');
-ga('require', 'displayfeatures');
-// Modifications:
-ga('set', 'checkProtocolTask', null); // Disables file protocol checking.
-ga('send', 'pageview', '/options'); // Set page, avoiding rejection due to chrome-extension protocol
+setUpGoogleAnalytics("/stop")
 
 try {
   const timerListDanger = createTimerList('timerListDanger', 'DANGER');
